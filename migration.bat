5659b61452871817eab95c851c247ff41503b492
Marouane <elmabroukmarouane@gmail.com>
2022-01-28 10:55:40 +0100

    Create My Dev Branch in Github

--- migration.bat
+++ migration.bat
@@ -7,7 +7,7 @@ echo dotnet ef --startup-project %2 migrations add %1
 dotnet ef --startup-project %2 migrations add %1
 
 echo -----------------------------------------
-echo *** Update Database to Project : %2 ***
+echo *** Update Database to Project : %2 *** 
 echo -----------------------------------------
 echo dotnet ef database update --startup-project %2
 dotnet ef database update --startup-project %2
